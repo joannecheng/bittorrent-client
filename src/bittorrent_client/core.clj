20fa871baf9049cb89728b7cfe78854d9bb2b028
Joanne Cheng <joanne@keen.io>
2018-06-04 20:25:08 -0600

    Holy crap, the tests pass

--- src/bittorrent_client/core.clj
+++ src/bittorrent_client/core.clj
@@ -4,6 +4,8 @@
             [clojure.tools.reader.reader-types :as rt]
             [clojure.string :as str]))
 
+(declare decode)
+
 ;; Bencoder
 (defn decode-number [stream delimiter & [result]]
   (let [c (str (rt/read-char stream))]
@@ -23,28 +25,53 @@
     (let [c (rt/peek-char stream)]
       (if (= c \e)
         result
-        (recur (conj result (decode stream)))))))
+        (recur (conj result (first (decode stream))))))))
 
 (defn decode-map [stream]
-  (apply assoc {} (decode-list stream)))
+  (apply hash-map (decode stream)))
 
 ;; The main decode function
-(defn decode [stream & i]
-  (let [indicator (if (nil? i) (rt/read-char stream) (first i))]
-    (case indicator
-      \d (decode-map stream)
-      \l (decode-list stream)
-      \i (decode-number stream "e")
-      (decode-string stream (decode-number stream ":" indicator))
-    )))
+;;(defn decode [stream & i]
+;;  (let [indicator (if (nil? i) (rt/read-char stream) (first i))]
+;;    (case indicator
+;;      \d (decode-map stream)
+;;      \l (decode-list stream)
+;;      \i (decode-number stream "e")
+;;      (decode-string stream (decode-number stream ":" indicator))
+;;    )))
 
 
-(def metainfo-stream
+(defn decode
+  ([stream]
+   (let [ch (rt/read-char stream)]
+     (decode stream ch)))
+
+  ([stream ch]
+   (loop [ch ch
+          result []]
+     (if (or (= ch \e) (nil? ch))
+       result
+       (let [item
+             (case ch
+               \d (decode-map stream)
+               \l (decode stream)
+               \i (decode-number stream "e")
+               (decode-string stream (decode-number stream ":" ch)))
+             next-ch (rt/read-char stream)
+             result (conj result item)]
+         (recur next-ch result)
+         )))))
+
+
+(defn metainfo-stream []
   (-> "video.torrent"
       io/resource
       io/file
       io/input-stream
       rt/input-stream-reader))
+
+(decode (metainfo-stream))
+
 
 ;; Main
 (defn -main
--- test/bittorrent_client/core_test.clj
+++ test/bittorrent_client/core_test.clj
@@ -1,15 +1,20 @@
 (ns bittorrent-client.core-test
   (:require [clojure.test :refer :all]
             [clojure.java.io :as io]
+            [clojure.tools.reader.reader-types :as rt]
             [bittorrent-client.core :refer :all]))
 
 
 (deftest decode-map-test
-  (testing "decode map"
-    (let [testcase (io/input-stream (.getBytes "3:cow3:moo4:spam4:eggse"))]
+  (testing "decode"
+    (let [testcase (->> (.getBytes "3:cow3:moo4:spam4:eggse")
+                        io/input-stream
+                        rt/input-stream-reader)]
     (is (= (bittorrent-client.core/decode-map testcase) {"cow" "moo", "spam" "eggs"})))))
 
 (deftest decode-number-test
   (testing "decode number"
-    (let [testcase (io/input-stream (.getBytes "8:"))]
+    (let [testcase (->> (.getBytes "8:")
+                        io/input-stream
+                        rt/input-stream-reader)]
       (is (= (bittorrent-client.core/decode-number testcase ":") 8)))))

4ea4054e056c4298aaae16ffb34c4685d0b2e379
Joanne Cheng <joanne@keen.io>
2018-06-04 19:39:40 -0600

    Got bencoding working

--- src/bittorrent_client/core.clj
+++ src/bittorrent_client/core.clj
@@ -1,44 +1,50 @@
 (ns bittorrent-client.core
   (:require [clojure.java.io :as io]
-            [clojure.string :as string]))
+            [clojure.string :as string]
+            [clojure.tools.reader.reader-types :as rt]
+            [clojure.string :as str]))
 
 ;; Bencoder
-(defn decode-number [stream delimiter & result]
-  (let [c (char (.read stream))]
-    (if (= (str c) delimiter)
+(defn decode-number [stream delimiter & [result]]
+  (let [c (str (rt/read-char stream))]
+    (if (= c delimiter)
       (read-string (str result))
-      (recur stream delimiter (str result c)))))
+      (decode-number stream delimiter (str result c)))))
 
 (defn decode-string [stream string-length & result]
-  (let [buffer (make-array Byte/TYPE string-length)]
-    (.read stream buffer)
-    (String. buffer)))
+  (let [c (rt/read-char stream)
+        new-result (str result c)]
+    (if (= (count new-result) string-length)
+      new-result
+      (recur stream string-length new-result))))
 
-(defn decode-map [stream & result]
+(defn decode-list [stream]
   (loop [result []]
-    (.mark stream 0)
-    (let [c (char (.read stream))]
-      (.reset stream)
+    (let [c (rt/peek-char stream)]
       (if (= c \e)
-        (apply assoc {} result)
-        (recur (conj result
-               (decode-string stream (decode-number stream ":"))))))))
+        result
+        (recur (conj result (decode stream)))))))
 
+(defn decode-map [stream]
+  (apply assoc {} (decode-list stream)))
 
 ;; The main decode function
-(defn decode [stream]
-  (let [indicator (.read stream)]
+(defn decode [stream & i]
+  (let [indicator (if (nil? i) (rt/read-char stream) (first i))]
     (case indicator
-      \d (decode-map (rest stream)))
-    ))
+      \d (decode-map stream)
+      \l (decode-list stream)
+      \i (decode-number stream "e")
+      (decode-string stream (decode-number stream ":" indicator))
+    )))
 
 
 (def metainfo-stream
   (-> "video.torrent"
-      (io/resource)
-      (io/file)
-      (io/input-stream)
-      ))
+      io/resource
+      io/file
+      io/input-stream
+      rt/input-stream-reader))
 
 ;; Main
 (defn -main
